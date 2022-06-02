b567fbd5438800a3c2c9728a78168eeb5403ecd6
devdevgoat <russell.w.lamb@gmail.com>
2022-06-02 14:59:27 -0400

    OOOoohhhh. just pass gamestop as provider

--- public/js/loop.js
+++ public/js/loop.js
@@ -13,12 +13,14 @@ class LoopringAPIClass{
     this.wsAPI = new sdk.WsAPI({ chainId });
     this.nftAPI = new sdk.NFTAPI({ chainId });
     this.delegate = new sdk.DelegateAPI({ chainId });
-    this.generateKeyPair = sdk.generateKeyPair();
+    //this.generateKeyPair = sdk.generateKeyPair();
     this.ConnectorNames = sdk.ConnectorNames;
     this.__chainId__ = chainId;
     this.contractAPI = sdk.ContractAPI;
     this.KEY_MESSAGE = sdk.KEY_MESSAGE;
+    this.generateKeyPair2 = sdk.generateKeyPair2;
     this.generateKeyPair = sdk.generateKeyPair;
+
   };
 }
 
@@ -28,7 +30,7 @@ class LoopringAccount {
     this.privateKey = '';//"491aecdb1d5f6400a6b62fd12a41a86715bbab675c37a4060ba115fecf94083c";
     this.accountId =  0;  //12454;
     this.accInfo = {}; //
-    this.exchangeAddress = '';  //"0x2e76EBd1c7c0C8e7c2B875b6d505a260C525d25e";
+    this.exchangeAPI = '';  //"0x2e76EBd1c7c0C8e7c2B875b6d505a260C525d25e";
     this.chainId = 1;
     this.nftTokenAddress =  '';  //"0x8394cB7e768070217592572582228f62CdDE4FCE";
     this.nftTokenId =  0;  //32768;
@@ -41,23 +43,29 @@ class LoopringAccount {
   }
 }
 
-async function signatureKeyPairMock(LOOPRING_EXPORTED_ACCOUNT, LoopringAPI,WEB3) {
-  console.log(WEB3.eth.personal);
-  const parms = {
-    web3: WEB3,
-    address: LOOPRING_EXPORTED_ACCOUNT.address,
-    keySeed:
-    LOOPRING_EXPORTED_ACCOUNT.accInfo.keySeed ??
-      LoopringAPI.KEY_MESSAGE.replace(
+async function signatureKeyPairMock(LOOPRING_EXPORTED_ACCOUNT, LoopringAPI) {
+  console.log(LOOPRING_EXPORTED_ACCOUNT.account);
+  let keyseed = ''
+  if (LOOPRING_EXPORTED_ACCOUNT.account.accInfo.keySeed != ''){
+      keyseed= LOOPRING_EXPORTED_ACCOUNT.account.accInfo.keySeed
+  } else {
+      keyseed = LoopringAPI.KEY_MESSAGE.replace(
         "${exchangeAddress}",
-        LOOPRING_EXPORTED_ACCOUNT.exchangeAddress
-      ).replace("${nonce}", (LOOPRING_EXPORTED_ACCOUNT.accInfo.nonce - 1).toString()),
+        LOOPRING_EXPORTED_ACCOUNT.exchangeAPI.exchangeInfo.exchangeAddress
+      ).replace("${nonce}", (LOOPRING_EXPORTED_ACCOUNT.account.accInfo.nonce - 1).toString())
+  }
+  const parms = {
+    web3: new Web3(window.gamestop),
+    address: LOOPRING_EXPORTED_ACCOUNT.address,
+    keySeed:keyseed,
     walletType: LoopringAPI.ConnectorNames.MetaMask,
     chainId: LoopringAPI.__chainId__,
   }
   console.log(parms);
   const eddsaKey = await LoopringAPI.generateKeyPair(parms); // can use gamestop.request({ method: 'personal_sign',params:["test","0xe5B6B887570Ae0EC87B379e1576C4fe0b892BA38",""] }) here I think
-  return eddsaKey;
+  //const sig = await window.gamestop.request({ method: 'personal_sign', params:[parms.keySeed,parms.address,""] });
+  //const eddsaKey = await LoopringAPI.generateKeyPair2(sig);
+  return eddsaKey;          
 }
 
 async function setupWeb3User(address) {
@@ -65,15 +73,16 @@ async function setupWeb3User(address) {
     // if (typeof WEB3 == 'undefined') var WEB3 = new Web3;
     let LoopringAPI = new LoopringAPIClass(sdk);
     const LOOPRING_EXPORTED_ACCOUNT = new LoopringAccount(address);
-    LOOPRING_EXPORTED_ACCOUNT.exchangeAddress = await LoopringAPI.exchangeAPI.getExchangeInfo()
-    LOOPRING_EXPORTED_ACCOUNT.accInfo = await LoopringAPI.exchangeAPI.getAccount({
+    LOOPRING_EXPORTED_ACCOUNT.exchangeAPI = await LoopringAPI.exchangeAPI.getExchangeInfo()
+    LOOPRING_EXPORTED_ACCOUNT.account = await LoopringAPI.exchangeAPI.getAccount({
         owner: LOOPRING_EXPORTED_ACCOUNT.address,
       });
     
     const eddsaKey = await signatureKeyPairMock(LOOPRING_EXPORTED_ACCOUNT, LoopringAPI,window.web3);
-    console.log("eddsaKey:", eddsaKey.sk);
+    console.log("eddsaKey:", eddsaKey);
+    console.log("Requesting api key")
     LOOPRING_EXPORTED_ACCOUNT.apiKey = await LoopringAPI.userAPI.getUserApiKey({
-      accountId: accInfo.accountId}, eddsaKey.sk
+      accountId: LOOPRING_EXPORTED_ACCOUNT.account.accInfo.accountId}, eddsaKey.sk
     );
     return await LOOPRING_EXPORTED_ACCOUNT;
 }
